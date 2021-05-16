3dfed3d25ea9445a904de7670b9c487fd3ad1e05
Harris Fotogiannis <hfotogiannis@gmail.com>
2021-05-16 21:35:17 +0300

    Add project files.

---
+++ PaintApp/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace PaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tomato;
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(165, 593);
+            this.panel1.TabIndex = 0;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(3, 353);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(159, 35);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "House(Roof)";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(3, 312);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(159, 35);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "House(Ceiling)";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(3, 271);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 35);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "House(Walls)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(12, 484);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(137, 24);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Pen width 15";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 424);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(127, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Pen width 5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(12, 454);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(137, 24);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Pen width 10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 514);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Color";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(3, 555);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(159, 35);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Clear";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(3, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Ellipse";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Rectangle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(3, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(3, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Freestyle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 34);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(240, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(971, 592);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ PaintApp/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Data.SQLite;
+
+namespace PaintApp
+{
+    public partial class Form1 : Form
+    {
+        String connectionString = "Data Source=PaintDB.db;Version=3;";
+        SQLiteConnection conn;
+        public int ch;
+        Graphics mg;
+        Pen p;
+        public int x1, x2, y1, y2;
+        public int m1, m2, n1, n2;
+        public int u1, u2, v1, v2;
+        public int g1, g2, f1, f2;
+        public bool drag;
+        public string d = DateTime.Now.ToString();
+        int countDown = 2;
+        int countDown2 = 2;
+        int countDown3 = 2;
+        Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            ch = 0;
+            mg = this.CreateGraphics();
+            p = new Pen(Color.Black);
+            drag = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ch = 1;
+            conn.Open();
+            String insertQuery = "Insert into Info(Shape, Timestamp) values('" + button1.Text + "','" + d + "')";
+            SQLiteCommand cmd = new SQLiteCommand(insertQuery, conn);
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ch = 2;
+            conn.Open();
+            String insertQuery = "Insert into Info(Shape, Timestamp) values('" + button2.Text + "','" + d + "')";
+            SQLiteCommand cmd = new SQLiteCommand(insertQuery, conn);
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DialogResult rs;
+            rs = colorDialog1.ShowDialog();
+            if (rs==DialogResult.OK)
+            {
+                p.Color = colorDialog1.Color;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            x1 = e.X;
+            y1 = e.Y;
+
+            if (radioButton1.Checked)
+            {
+                p.Width = 5;
+            }
+            if (radioButton2.Checked)
+            {
+                p.Width = 10;
+            }
+            if (radioButton3.Checked)
+            {
+                p.Width = 15;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+            if (ch == 2)
+            {
+                if (x2 - x1 == y2 - y1)
+                {
+                    mg.DrawEllipse(p, x1, y1, x2 - x1, y2 - y1);
+                }
+            }
+            else if (ch == 3)
+            {
+                mg.DrawLine(p, x1, y1, x2, y2);
+            }
+            else if (ch == 4)
+            {
+                mg.DrawRectangle(p, x1, y1, x2 - x1, y2 - y1);
+            }
+            else if (ch == 5)
+            {
+                mg.DrawEllipse(p, x1, y1, x2 - x1, y2 - y1);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+
+            if(drag == true && ch == 1)
+            {
+                mg.DrawLine(p, x1, y1, x2, y2);
+                x1 = x2;
+                y1 = y2;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            mg.Clear(Color.White);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SQLiteConnection(connectionString);
+            random = new Random();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g1 += 250;
+            f1 = 150;
+            g2 = g1;
+            f2 = 300;
+            mg.DrawLine(p, g1, f1, g2, f2);
+            countDown--;          
+            if (countDown == 0)
+            {
+                timer1.Enabled = false;
+                countDown = 2;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (timer3.Enabled)
+                timer3.Enabled = false;
+            else
+                timer3.Enabled = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            u1 = 375;
+            v1 = 75;
+            u2 += 250;
+            v2 = 150;
+            mg.DrawLine(p, u1, v1, u2, v2);
+            countDown3--;
+            if (countDown3 == 0)
+            {
+                timer3.Enabled = false;
+                countDown3 = 2;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                timer1.Enabled = false;
+            else
+                timer1.Enabled = true;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled)
+                timer2.Enabled = false;
+            else
+                timer2.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {      
+            m1 = 250;
+            n1 += 150;
+            n2 = n1;
+            m2 = 500;
+            mg.DrawLine(p, m1, n1, m2, n2);
+            countDown2--;
+            if (countDown2 == 0)
+            {
+                timer2.Enabled = false;
+                countDown2 = 2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ch = 3;
+            conn.Open();
+            String insertQuery = "Insert into Info(Shape, Timestamp) values('" + button3.Text + "','" + d + "')";
+            SQLiteCommand cmd = new SQLiteCommand(insertQuery, conn);
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ch = 4;
+            conn.Open();
+            String insertQuery = "Insert into Info(Shape, Timestamp) values('" + button4.Text + "','" + d + "')";
+            SQLiteCommand cmd = new SQLiteCommand(insertQuery, conn);
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ch = 5;
+            conn.Open();
+            String insertQuery = "Insert into Info(Shape, Timestamp) values('" + button5.Text + "','" + d + "')";
+            SQLiteCommand cmd = new SQLiteCommand(insertQuery, conn);
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
